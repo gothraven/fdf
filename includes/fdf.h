d3e5b72b0e13edd03898a87aa301ba1352feaa27
szaghbane <safiy-errahmane.zaghbane@etu.u-pec.fr>
2018-09-29 22:18:23 +0200

    [ADD] Add Makefile and setup main.c to start work

---
+++ includes/fdf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/29 21:58:46 by szaghban          #+#    #+#             */
+/*   Updated: 2018/09/29 21:58:48 by szaghban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <math.h>
+# include "../libft/libft.h"
+# include "../minilibx_macos/mlx.h"
+# include <stdio.h>
+
+#endif
