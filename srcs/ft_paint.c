3b9183c09d962bc0515d997fa7fcfe9aa1b84653
szaghbane <safiy-errahmane.zaghbane@etu.u-pec.fr>
2018-10-27 17:57:51 +0200

    [ADD] Add status show in the top left screen

--- srcs/ft_paint.c
+++ srcs/ft_paint.c
@@ -6,7 +6,7 @@
 /*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/02 23:26:51 by gothraven         #+#    #+#             */
-/*   Updated: 2018/10/13 21:51:54 by szaghban         ###   ########.fr       */
+/*   Updated: 2018/10/27 17:57:09 by szaghban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,32 @@ void	ft_update_2d_pixels(t_img *image)
 		image->pixels[i].x = 11 * image->pixels[i].x + WIN_WIDTH / 2;
 		image->pixels[i].y = 11 * image->pixels[i].y + WIN_HEIGHT / 2;
 	}
+}
+
+void	ft_paint_screen_status(t_img *image)
+{
+	void	*mlx_ptr;
+	void	*mlx_win_ptr;
+	int		i;
+	char	*status[11];
+
+	status[0] = ft_strjoin("scale: ", ft_itoa((int)image->settings.scale));
+	status[1] = ft_strjoin("x rotation: ", ft_ftoa(image->settings.xrot, 2));
+	status[2] = ft_strjoin("y rotation: ", ft_ftoa(image->settings.yrot, 2));
+	status[3] = ft_strjoin("x move: ", ft_ftoa(image->settings.xmove, 2));
+	status[4] = ft_strjoin("y move: ", ft_ftoa(image->settings.ymove, 2));
+	status[5] = "projection: ISO";
+	status[6] = "[UP DOWN LEFT RIGHT] move map arround";
+	status[7] = "[A || D] rotate y left or right";
+	status[8] = "[W || S] rotate x up or down";
+	status[9] = "[E || Q] scale more or less";
+	status[10] = NULL;
+	mlx_ptr = image->window->mlx;
+	mlx_win_ptr = image->window->mlx_win;
+	i = -1;
+	while (status[++i])
+		mlx_string_put(mlx_ptr, mlx_win_ptr,
+				20, i * 20, 0xffffff, status[i]);
 }
 
 void	ft_camera_prespective(t_img *image)
@@ -53,6 +79,7 @@ int		ft_paint(t_img *image)
 
 	ft_camera_prespective(image);
 	ft_update_2d_pixels(image);
+	ft_paint_screen_status(image);
 	i = -1;
 	while (++i < image->size)
 	{
