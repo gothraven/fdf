d910b94062f270bdf796e2944e2d999b6c9e33d2
gothraven <szaghbane@gmail.com>
2019-01-28 19:16:18 +0100

    [ADD] srcs and includes

---
+++ srcs/main.c
@@ -0,0 +1,28 @@
+#include "fdf.h"
+
+int		main(int argc, char **argv)
+{
+	int			fd;
+	t_map		*map;
+	t_coord_val	*coords_stack;
+	t_fdf		*fdf;
+
+	errno = 0;
+	coords_stack = NULL;
+	if (argc == 2)
+	{
+		if (!((fd = open(argv[1], O_RDONLY)) >= 0))
+			terminate(ERR_MAP);
+		map = map_init();
+		if (read_map(fd, &coords_stack, map) == -1)
+			terminate(ERR_MAP_READING);
+		fdf = fdf_init(map);
+		stack_to_arrays(&coords_stack, map);
+		fdf->camera = camera_init(fdf);
+		draw(fdf->map, fdf);
+		setup_controls(fdf);
+		mlx_loop(fdf->mlx);
+	}
+	terminate(ERR_USAGE);
+	return (0);
+}
